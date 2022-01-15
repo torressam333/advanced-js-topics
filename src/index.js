be933aa54c1afcb72c06a44a873cb089131d6cf9
sam <samuel.torres@goodwillaz.org>
2022-01-15 09:26:50 -0700

    Add higher order functions example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 //This file is used for mutli file script execution
-import Memoization from "./topics/memoization.js";
-import CurryingJS from "./topics/currying.js";
-import GetUsers from "./topics/async-await-with-fetch";
-import { fullName, homeCountry } from "./topics/object-descructuring.js";
+// import Memoization from "./topics/memoization.js";
+// import CurryingJS from "./topics/currying.js";
+// import GetUsers from "./topics/async-await-with-fetch";
+// import { fullName, homeCountry } from "./topics/object-descructuring.js";
+import HOF from "./topics/higher-order-functions";
+
