a1a791900fe5fd2e7ca4a112efa50bed9128070a
Samuel <torressam333@gmail.com>
2022-01-30 09:19:05 -0700

    Add file for array data structure overview

---
+++ src/topics/array_usage.js
@@ -0,0 +1,34 @@
+//Arrays can have mixed types and duplicates
+const names = ["Sam", "John", "Jordan", 29, "John"];
+
+//Get first index === 'john'
+console.log(names[1]);
+
+//Loop through arrays
+for (const el of names) {
+  console.log(el);
+}
+
+//Add new items to original array
+names.push("Rick");
+
+console.log(names);
+
+//Concat a new name w/o changing original array
+const newNamesArray = names.concat("Sue");
+
+console.log(names); //Sue will not appear
+console.log(newNamesArray); //Sue will appear
+
+/*Deletion and Finding elements in Arrays*/
+
+//Finding is performance intensive b/c of has to search the entire array
+const johnsIndex = names.findIndex((el) => el === "John");
+//Returns 1 and not 1,4 as it stops execition on the first finding
+console.log(johnsIndex);
+
+//Deleting is also performance intensive
+
+//When an item is removed the entire array has to shift indices
+names.splice(3, 1); //Removes 29
+console.log(names);
