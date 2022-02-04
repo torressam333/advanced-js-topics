d1f9b7152fdb97e9c646f730737936915a08b138
Samuel <torressam333@gmail.com>
2022-02-04 15:06:24 -0700

    Create async fetch call to swapi api

--- src/topics/debouncer.js
+++ src/topics/debouncer.js
@@ -7,4 +7,35 @@
  * We can limit how often the request fires by implementing a debouncer
  */
 
-const debouncer = ((time) => {}, time);
+// const debouncer =
+//   (() => {
+//     let counter = 0;
+//     const hoverDiv = document.querySelector(".debounce-container");
+
+//     hoverDiv.addEventListener("mouseenter", () => {
+//       return counter++;
+//     });
+//   },
+//   3000);
+
+const swapiApiCall = async () => {
+  const response = await fetch(`https://swapi.dev/api/planets/${id}`);
+
+  //I can do an inline return but want to give this
+  //variable a cool name :)
+  const starWarsPlanet = await response.json();
+
+  return starWarsPlanet;
+};
+
+const debouncer = () => {
+  const hoverDiv = document.querySelector(".debounce-container");
+
+  //As the user mouses over fire an event
+  hoverDiv.addEventListener("mouseover", () => {
+    //Make api call when mouseover happens (this will end badly without debounce)
+    swapiApiCall();
+  });
+};
+
+console.log(debouncer());
